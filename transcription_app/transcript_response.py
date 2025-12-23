0f3286d4b28bbe7e2313a746c8c2be510a4aa393
Celina <celinalouissaint@gmail.com>
2025-12-22 19:48:09 -0800

    Update UI

--- transcription_app/transcript_response.py
+++ transcription_app/transcript_response.py
@@ -1,19 +1,23 @@
 import json
 import os
 import logging
+from datetime import timedelta
 
 def transcription_response(whisper_response, summary_response):
     transcript_file = os.path.join('output/transcripts', 'transcript.json')
     try:
         transcription_segments = []
         for segment in whisper_response.segments:
+            start_time = str(timedelta(seconds=segment["start"])).split('.')[0]
+            end_time = str(timedelta(seconds=segment["end"])).split('.')[0]
+            
             transcription_data = {
-                            "id": segment["id"],
-                            "start": round(segment["start"], 2),
-                            "end": round(segment["end"], 2),
-                            "speaker": segment["speaker"],
-                            "text": segment["text"]
-                        }
+                "id": segment["id"],
+                "start": start_time,
+                "end": end_time,
+                "speaker": segment["speaker"],
+                "text": segment["text"]
+            }
             transcription_segments.append(transcription_data)
 
         final_transcript = {
